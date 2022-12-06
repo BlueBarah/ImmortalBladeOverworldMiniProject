2c5d68763dfea1faa69e1d7089728fe24bfef84e
Ian Beard <ianbeard0159@gmail.com>
2022-12-05 17:06:40 -0800

    Applied animation to Jason

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,6 +8,10 @@ public class Mover : MonoBehaviour
     [SerializeField] float ySpeed; //usually 0, could be useful for flying enemies later tho
     protected SpriteRenderer sprite;
 
+    // Animation Conditions
+    private Animator animator;
+    public bool isRunning { get; private set;}
+
     //TODO:
     //-LOS cone for detection (probably based on movement direction)
     //-collision box/range for battle engagement
@@ -17,12 +21,29 @@ public class Mover : MonoBehaviour
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        isRunning = false;
     }
 
     //Move in the direction given, also flips sprite when moving to the left
     //TODO: maybe change movement system to rb.movePosition
     protected void Move(Vector3 direction)
     {
+        // Animation Conditions
+        if (direction != Vector3.zero) {
+            isRunning = true;
+        }
+        else {
+            isRunning = false;
+        }
+
+        if (animator != null) {
+            animator.SetBool("isRunning", isRunning);
+        }
+        else {
+            animator = GetComponent<Animator>();
+        }
+
         Vector3 moveDelta = new Vector3(direction.x * walkingSpeed, direction.y * ySpeed, direction.z * walkingSpeed);
 
         //Mover is moving to right, unflip sprite
